34e1ce08b89b7899e6e49d4cd05423009fa1d32f
unknown <SharmilaPuligundla2002@gmail.com>
2024-08-15 22:19:59 +0530

    error

---
+++ whatsapp-clone/convex/auth.config.ts
@@ -0,0 +1,12 @@
+// https://stable-pegasus-62.clerk.accounts.dev
+const authConfig = {
+ 
+    providers : [
+        {
+            domain : "https://stable-pegasus-62.clerk.accounts.dev",
+            applicationID : "convex" , 
+        },
+    ]
+};
+
+export default authConfig;
---
+++ whatsapp-clone/convex/users.ts
@@ -0,0 +1,30 @@
+import { ConvexError, v } from "convex/values";
+import { internalMutation, query } from "./_generated/server";
+
+export const createUser = internalMutation({
+	args: {
+		tokenIdentifier: v.string(),
+		email: v.string(),
+		name: v.string(),
+		image: v.string(),
+	},
+	handler: async (ctx, args) => {
+		await ctx.db.insert("users", {
+			tokenIdentifier: args.tokenIdentifier,
+			email: args.email,
+			name: args.name,
+			image: args.image,
+			isOnline: true,
+		});
+	},
+});
+
+export const setUserOffline = internalMutation({
+  args:{tokenIdentifier: v.string()},
+  handler: async (ctx,args) => {
+    const user = await ctx.db
+    .query("users")
+    .withIndex("by_tokenIdentifier", q => q.eq("tokenIdentifier",args.tokenIdentifier))
+
+  }
+});
