72f845a25ee3a1ba5bc3467a264367946cc6dc58
unknown <SharmilaPuligundla2002@gmail.com>
2024-08-18 20:48:40 +0530

    getmyconversations query added

--- whatsapp-clone/src/components/home/left-panel.tsx
+++ whatsapp-clone/src/components/home/left-panel.tsx
@@ -3,24 +3,39 @@ import { ListFilter, Search } from "lucide-react";
 import { Input } from "../ui/input";
 import ThemeSwitch from "./theme-switch";
 import Conversation from "./conversation";
-import { conversations }from "../../dummy-data/db"; 
 import { UserButton } from "@clerk/nextjs";
+
 import UserListDialog from "./user-list-dialog";
-import { useConvexAuth } from "convex/react";
+import { useConvexAuth, useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { useEffect } from "react";
+import { useConversationStore } from "@/store/chat-store";
 
 const LeftPanel = () => {
-	const {isAuthenticated} = useConvexAuth();
+	const { isAuthenticated, isLoading } = useConvexAuth();
+	const conversations = useQuery(api.conversations.getMyConversations, isAuthenticated ? undefined : "skip");
+
+	const { selectedConversation, setSelectedConversation } = useConversationStore();
+
+	useEffect(() => {
+		const conversationIds = conversations?.map((conversation) => conversation._id);
+		if (selectedConversation && conversationIds && !conversationIds.includes(selectedConversation._id)) {
+			setSelectedConversation(null);
+		}
+	}, [conversations, selectedConversation, setSelectedConversation]);
+
+	if (isLoading) return null;
+
 	return (
 		<div className='w-1/4 border-gray-600 border-r'>
 			<div className='sticky top-0 bg-left-panel z-10'>
 				{/* Header */}
 				<div className='flex justify-between bg-gray-primary p-3 items-center'>
-					<UserButton/>
-					
+					<UserButton />
+
 					<div className='flex items-center gap-3'>
-					{isAuthenticated && <UserListDialog />}
+						{isAuthenticated && <UserListDialog />}
 						<ThemeSwitch />
-						
 					</div>
 				</div>
 				<div className='p-3 flex items-center'>
@@ -42,14 +57,15 @@ const LeftPanel = () => {
 
 			{/* Chat List */}
 			<div className='my-3 flex flex-col gap-0 max-h-[80%] overflow-auto'>
-				{/* Rendering Conversations */}
+				{/* Conversations will go here*/}
 				{conversations?.map((conversation) => (
-					<Conversation key={conversation._id} conversation={conversation} />
+					<Conversation key={conversation._id} conversation={conversation}  />
 				))}
+
 				{conversations?.length === 0 && (
 					<>
 						<p className='text-center text-gray-500 text-sm mt-3'>No conversations yet</p>
-						<p className='text-center text-gray-500 text-sm mt-3'>
+						<p className='text-center text-gray-500 text-sm mt-3 '>
 							We understand {"you're"} an introvert, but {"you've"} got to start somewhere 😊
 						</p>
 					</>
@@ -58,5 +74,4 @@ const LeftPanel = () => {
 		</div>
 	);
 };
-
 export default LeftPanel;
