cee59858895921c0778e6062ecec9a1d195aaa39
unknown <SharmilaPuligundla2002@gmail.com>
2024-08-21 20:23:49 +0530

    4_getMessage query added

--- whatsapp-clone/convex/messages.ts
+++ whatsapp-clone/convex/messages.ts
@@ -41,7 +41,155 @@ export const sendTextMessage = mutation({
 			content: args.content,
 			conversation: args.conversation,
 			messageType: "text",
-        
-        });
-    },
+		});
+
+		// TODO => add @gpt check later
+		// if (args.content.startsWith("@gpt")) {
+		// 	// Schedule the chat action to run immediately
+		// 	await ctx.scheduler.runAfter(0, api.openai.chat, {
+		// 		messageBody: args.content,
+		// 		conversation: args.conversation,
+		// 	});
+		// }
+
+		// if (args.content.startsWith("@dall-e")) {
+		// 	await ctx.scheduler.runAfter(0, api.openai.dall_e, {
+		// 		messageBody: args.content,
+		// 		conversation: args.conversation,
+		// 	});
+		// }
+	},
 });
+
+export const sendChatGPTMessage = mutation({
+	args: {
+		content: v.string(),
+		conversation: v.id("conversations"),
+		messageType: v.union(v.literal("text"), v.literal("image")),
+	},
+	handler: async (ctx, args) => {
+		await ctx.db.insert("messages", {
+			content: args.content,
+			sender: "ChatGPT",
+			messageType: args.messageType,
+			conversation: args.conversation,
+		});
+	},
+});
+
+// Optimized
+export const getMessages = query({
+	args: {
+		conversation: v.id("conversations"),
+	},
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
+		if (!identity) {
+			throw new Error("Unauthorized");
+		}
+
+		const messages = await ctx.db
+			.query("messages")
+			.withIndex("by_conversation", (q) => q.eq("conversation", args.conversation))
+			.collect();
+
+		const userProfileCache = new Map();
+
+		const messagesWithSender = await Promise.all(
+			messages.map(async (message) => {
+				if (message.sender === "ChatGPT") {
+					const image = message.messageType === "text" ? "/gpt.png" : "dall-e.png";
+					return { ...message, sender: { name: "ChatGPT", image } };
+				}
+				let sender;
+				// Check if sender profile is in cache
+				if (userProfileCache.has(message.sender)) {
+					sender = userProfileCache.get(message.sender);
+				} else {
+					// Fetch sender profile from the database
+					sender = await ctx.db
+						.query("users")
+						.filter((q) => q.eq(q.field("_id"), message.sender))
+						.first();
+					// Cache the sender profile
+					userProfileCache.set(message.sender, sender);
+				}
+
+				return { ...message, sender };
+			})
+		);
+
+		return messagesWithSender;
+	},
+});
+
+export const sendImage = mutation({
+	args: { imgId: v.id("_storage"), sender: v.id("users"), conversation: v.id("conversations") },
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
+		if (!identity) {
+			throw new ConvexError("Unauthorized");
+		}
+
+		const content = (await ctx.storage.getUrl(args.imgId)) as string;
+
+		await ctx.db.insert("messages", {
+			content: content,
+			sender: args.sender,
+			messageType: "image",
+			conversation: args.conversation,
+		});
+	},
+});
+
+export const sendVideo = mutation({
+	args: { videoId: v.id("_storage"), sender: v.id("users"), conversation: v.id("conversations") },
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
+		if (!identity) {
+			throw new ConvexError("Unauthorized");
+		}
+
+		const content = (await ctx.storage.getUrl(args.videoId)) as string;
+
+		await ctx.db.insert("messages", {
+			content: content,
+			sender: args.sender,
+			messageType: "video",
+			conversation: args.conversation,
+		});
+	},
+});
+
+// unoptimized
+
+// export const getMessages = query({
+// 	args:{
+// 		conversation: v.id("conversations"),
+// 	},
+// 	handler: async (ctx, args) => {
+// 		const identity = await ctx.auth.getUserIdentity();
+// 		if (!identity) {
+// 			throw new ConvexError("Not authenticated");
+// 		}
+
+// 		const messages = await ctx.db
+// 		.query("messages")
+// 		.withIndex("by_conversation", q=> q.eq("conversation", args.conversation))
+// 		.collect();
+
+// 		// john => 200 , 1
+// 		const messagesWithSender = await Promise.all(
+// 			messages.map(async (message) => {
+// 				const sender = await ctx.db
+// 				.query("users")
+// 				.filter(q => q.eq(q.field("_id"), message.sender))
+// 				.first();
+
+// 				return {...message,sender}
+// 			})
+// 		)
+
+// 		return messagesWithSender;
+// 	}
+// });
