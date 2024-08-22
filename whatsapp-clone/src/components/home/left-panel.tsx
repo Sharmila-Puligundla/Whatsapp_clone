32671c307cd41a56cef7b2c1dda10b7219f4f8bc
unknown <SharmilaPuligundla2002@gmail.com>
2024-08-22 11:49:33 +0530

    kickuser mutation added

--- whatsapp-clone/src/components/home/left-panel.tsx
+++ whatsapp-clone/src/components/home/left-panel.tsx
@@ -59,7 +59,7 @@ const LeftPanel = () => {
 			<div className='my-3 flex flex-col gap-0 max-h-[80%] overflow-auto'>
 				{/* Conversations will go here*/}
 				{conversations?.map((conversation) => (
-					<Conversation key={conversation._id} conversation={conversation}  />
+					<Conversation key={conversation._id} conversation={conversation} />
 				))}
 
 				{conversations?.length === 0 && (
