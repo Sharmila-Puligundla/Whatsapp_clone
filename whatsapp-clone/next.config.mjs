fa73a847074fcfa90ae38bca36a0c5368f39b60e
unknown <SharmilaPuligundla2002@gmail.com>
2024-08-22 11:34:11 +0530

    send images and videos mutation added

--- whatsapp-clone/next.config.mjs
+++ whatsapp-clone/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{ hostname: "striped-robin-951.convex.cloud" }],
+	},
+};
 
 export default nextConfig;
